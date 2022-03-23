613da021b55f1e1961c66b3a9862bc600f7f301d
user <user@A401-PC46>
2022-03-23 15:50:39 +0800

    加入專案檔案。

---
+++ 20220323-A/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20220323_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("階層=");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int result = 1;
+                for (int i = 1; i <= n; i++) 
+            {
+                result = result * i;
+            }
+            Console.WriteLine("{0}!={1}", n, result);
+            Console.ReadLine();
+        }
+    }
+}
